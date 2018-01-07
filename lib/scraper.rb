f779624299f3fc6f77368400f6547a49b3f68d95
Aaron Lieberman <aaronmaxmusic@gmail.com>
2018-01-07 04:33:14 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,9 +4,9 @@ require 'open-uri'
 
 class Scraper
 
-    index_url = Nokogiri::HTML(open("./fixtures/student-site/index.html"))
-
   def self.scrape_index_page(index_url)
+
+    index_url = Nokogiri::HTML(open("./fixtures/student-site/index.html"))
 
     index_url.css(".card-text-container").text
 
