3e42911894e311398a5290ea81522b6a1754fa34
Aaron Lieberman <aaronmaxmusic@gmail.com>
2018-01-07 04:30:15 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,9 +7,9 @@ index_url = Nokogiri::HTML(open("./fixtures/student-site/index.html"))
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
-    doc.css(".card-text-container").text
-                
+
+    index_url.css(".card-text-container").text
+
   end
 
   def self.scrape_profile_page(profile_url)
