4e1512fcc561e8d337a567bd02a8cdc657904470
Aaron Lieberman <aaronmaxmusic@gmail.com>
2018-01-07 06:36:12 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,11 +7,12 @@ class Scraper
 
   def self.scrape_index_page(index_url)
 
-    index_url = Nokogiri::HTML(open("./fixtures/student-site/index.html"))
+    index_url = Nokogiri::HTML(open(index_url))
 
     students = []
 
-    index_url.css(".student-name").each do |student|
+    index_url.css("div.roster-cards-container").each do |card|
+      card.css(".student_card a") .each do |student|
       student_name = student.css(".student-name").text
       student_location = student.css(".student_location").text
       student_profile_link = ".fixtures/student-site/#{student.attr('href')}"
