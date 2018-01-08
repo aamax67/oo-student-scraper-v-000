8bea1b909264348d5fecbb2548ffd6d86753469d
Aaron Lieberman <aaronmaxmusic@gmail.com>
2018-01-08 04:19:24 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,7 @@ class Scraper
 
     index_url = Nokogiri::HTML(open(index_url))
 
-    students = []
+        students = []
 
       index_url.css("div.roster-cards-container").each do |card|
         card.css(".student-card a").each do |student|
@@ -23,6 +23,37 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
+
+      profile_url = Nokogiri::HTML(open(profile_url))
+
+        student_profiles = {}
+
+      links = profile_url.css(".social-icon-container").children.css("a").map {|el| el.attribute("href").value}
+      
+        links.each do |link|
+
+        if links.include?("linkedin")
+          student_profiles[:linkedin] = link
+        elsif links.include?("github")
+          student_profiles[:github] = link
+        elsif links.include?("twitter")
+          student_profiles[:twitter] = link
+        else 
+          student_profiles[:blog] = link
+        end
+      end
+      student_profiles
+    end
+  end
+      
+        
+        
+
+  
+
+    
+
+    
 
   end
 
